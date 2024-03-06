4de1d4940263052ff62e9396bc10999765944b9e
MeganHancock <mgnhnck@gmail.com>
2024-03-05 17:16:14 -0700

    init

---
+++ client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'dev-ut6mkremlurkels2.us.auth0.com'
+export const audience = 'https://mgnhnck.com'
+export const clientId = 'TnBPD2zfqVOsZbhp1FP3XYfkkElhsfaE'
