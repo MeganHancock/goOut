51df384f364f2caf01de6a19f1483e64bda17fd7
MeganHancock <mgnhnck@gmail.com>
2024-03-07 22:31:19 -0700

    gettin comments for event

--- server/src/controllers/TowerEventController.js
+++ server/src/controllers/TowerEventController.js
@@ -2,6 +2,8 @@ import { Auth0Provider } from "@bcwdev/auth0provider";
 import BaseController from "../utils/BaseController.js";
 import { towerEventsService } from "../services/TowerEventsService.js";
 import { ticketsService } from "../services/TicketsService.js";
+import { commentsService } from "../services/CommentsService.js";
+import { CommentSchema } from "../models/Comment.js";
 
 export class TowerEventController extends BaseController {
     constructor() {
@@ -10,6 +12,7 @@ export class TowerEventController extends BaseController {
             .get('', this.getAllTowerEvents)
             .get('/:eventId', this.getTowerEventById)
             .get('/:eventId/tickets', this.getAllTicketsToOneEvent)
+            .get('/:eventId/comments', this.getCommentsForEvent)
             .use(Auth0Provider.getAuthorizedUserInfo)
             .post('', this.createTowerEvent)
             .put('/:eventId', this.updateTowerEvent)
@@ -73,6 +76,16 @@ export class TowerEventController extends BaseController {
             const eventId = request.params.eventId
             const tickets = await ticketsService.getAllTicketsToOneEvent(eventId)
             response.send(tickets)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async getCommentsForEvent(request, response, next) {
+        try {
+            const eventId = request.params.eventId
+            const comments = await commentsService.getCommentsForEvent(eventId)
+            response.send(comments)
         } catch (error) {
             next(error)
         }
--- server/src/services/CommentsService.js
+++ server/src/services/CommentsService.js
@@ -1,6 +1,10 @@
 import { dbContext } from "../db/DbContext.js"
 
 class CommentsService {
+    async getCommentsForEvent(eventId) {
+        const comments = await dbContext.Comments.find({ eventId }).populate('creator', 'name picture')
+        return comments
+    }
     async createComment(commentData) {
         const comment = await dbContext.Comments.create(commentData)
         await comment.populate('creator', 'name picture')
